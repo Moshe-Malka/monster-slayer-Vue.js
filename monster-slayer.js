122f62bdc20bc3ecd911145a665aa859dd8a7851
moshe malka <moshemalka2014@gmail.com>
2018-05-23 01:24:11 +0300

    Add files via upload

--- monster-slayer.js
+++ monster-slayer.js
@@ -1,101 +1,96 @@
 new Vue({
   el: '#app',
   data: {
-    youHealthBar: 100,
+    playerHealthBar: 100,
     monsterHealthBar: 100,
     newGameStarted: false,
    	logs: []
   },
   methods:{
-  	logMonsterAttack(amount){
-    	this.logs.push("MONSTER hit PLAYER for " + amount);
+    // logAttacks: function(){
+    //   return;
+    // },
+  	// logMonsterAttack(amount){
+    // 	this.logs.unshift("MONSTER hit PLAYER for " + amount);
+    // },
+    // logPlayerAttack(amount){
+    // 	this.logs.unshift("PLAYER hit MONSTER for " + amount);
+    // },
+    getRandom: function(min,max){
+      return Math.max(Math.floor(Math.random() * max) +1, min);
     },
-    logPlayerAttack(amount){
-    	this.logs.push("PLAYER hit MONSTER for " + amount);
+    checkWinLose: function(){
+      if(this.monsterHealthBar <= 0){
+        this.giveUp();
+				alert("Game Over - You Win !");
+        return true;
+      } else if(this.playerHealthBar <= 0){
+        this.giveUp();
+				alert("Game Over - You Lose !");
+        return true;
+      }else{
+        return false;
+      }
     },
   	attack: function(){
-    	var rand1 = Math.floor(Math.random() * 30);
-    	var rand2 = Math.floor(Math.random() * 15);
-    	var monsterElement = document.querySelector("#monster-healthbar	> .healthbar");
-      var youElement = document.querySelector("#you-healthbar	> .healthbar");
-
-      var newScore1 = this.monsterHealthBar - rand1;
-      var newScore2 = this.monsterHealthBar - rand2;
-      var vm = this;
-      if(newScore1 < 1){
-      	this.monsterHealthBar = 0;
-      	monsterElement.style.width = '0%';
-        setTimeout(function(){
-        	vm.giveUp();
-					alert("Game Over - You Win !")
-					}, 1000);
-      }else if(newScore2 <1){
-      	this.youHealthBar = 0;
-      	youElement.style.width = '0%';
-        setTimeout(function(){
-        	vm.giveUp();
-					alert("Game Over - You Lose !")
-					}, 1000);
-      }else{
-      	// attacking monster
-        this.logPlayerAttack(rand1);
-      	monsterElement.style.width = newScore1 + '%';
-      	this.monsterHealthBar = newScore1;
-
-        // monster attacks back
-        this.logMonsterAttack(rand2);
-      	youElement.style.width = newScore2 + '%';
-      	this.youHealthBar = newScore2;
+      var monsterDamage = this.getRandom(3,15);
+      this.monsterHealthBar -= monsterDamage;
+      this.logs.unshift({
+        isPlayer: true,
+        text: 'PLAYER hit MONSTER for ' + monsterDamage
+      });
+      if(this.checkWinLose()){
+        return;
       }
+      var playerDamage = this.getRandom(2,8);
+      this.playerHealthBar -= playerDamage;
+      this.logs.unshift({
+        isPlayer: false,
+        text: 'MONSTER hit PLAYER for ' + playerDamage
+      });
+      this.checkWinLose();
     },
     heal: function(){
-    	var rand = Math.floor(Math.random() * 30);
-    	var elem = document.querySelectorAll("#you-healthbar	> .healthbar")[0];
-      var newScore = this.youHealthBar + rand;
-      if(newScore > 100) newScore = 100;
-      elem.style.width = newScore + '%';
-      this.youHealthBar = newScore;
+      var healingAmount = this.getRandom(5,12);
+      if(this.playerHealthBar + healingAmount > 100){
+        this.playerHealthBar = 100;
+        return;
+      }
+      this.playerHealthBar += healingAmount;
+      this.logs.unshift({
+        isPlayer: true,
+        text: 'Player heals for ' + healingAmount
+      });
     },
     specialAttack: function(){
-    	var youElem = document.querySelectorAll("#you-healthbar	> .healthbar")[0];
-      var monsterElem = document.querySelectorAll("#monster-healthbar	> .healthbar")[0];
-			var newScore1 = this.monsterHealthBar - 35;
-			var newScore2 = this.youHealthBar - 15;
-      // monster lost - game over
-      if( newScore1 < 0 ) {
-      	this.monsterHealthBar = 0;
-      	youElem.style.width = '0%';
-        this.giveUp();
-				alert("Game Over - You WIN !");
-      } else if( newScore2 < 0 ){
-      	// you lost - game over
-      	this.youHealthBar = 0;
-      	youElem.style.width = '0%';
-				this.giveUp();
-				alert("Game Over - You LOSE !");
-      }else{
-      	this.logPlayerAttack(35);
-      	// attacking monster (35 points)
-	      monsterElem.style.width = newScore1 + '%';
-	      this.monsterHealthBar = newScore1;
-
-      	this.logMonsterAttack(15);
-	      // monster attavking back (15 points)
-	      youElem.style.width = newScore2 + '%';
-	      this.youHealthBar = newScore2;
+      var monsterDamage = this.getRandom(8,25);
+      this.monsterHealthBar -= monsterDamage;
+      this.logs.unshift({
+        isPlayer: true,
+        text: 'PLAYER hit MONSTER HARD for ' + monsterDamage
+      });
+      if(this.checkWinLose()){
+        return;
       }
+      var playerDamage = this.getRandom(8,18);
+      this.playerHealthBar -= playerDamage;
+      this.logs.unshift({
+        isPlayer: false,
+        text: 'MONSTER hit PLAYER HARD for ' + playerDamage
+      });
+      this.checkWinLose();
     },
     giveUp: function(){
-    	this.newGameStarted = !this.newGameStarted;
+    	this.newGameStarted = false;
       this.logs = [];
-      var elements = document.querySelectorAll('.healthbar.text-center');
-    	Array.from(elements).forEach( hb => hb.style.width = (100 + '%'));
       this.monsterHealthBar = 100;
-      this.youHealthBar = 100;
+      this.playerHealthBar = 100;
     },
     startNewGame: function(){
-    this.newGameStarted = !this.newGameStarted;
-    if( !this.newGameStarted ) this.giveUp();
+    this.newGameStarted = true;
+    // this.monsterHealthBar = 100;
+    // this.playerHealthBar = 100;
+    // this.logs = [];
     }
   }
 })
